f92dfb59ee08696463b6e6514afa78f4a60887fa
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-08 00:08:04 +0200

    Solved Challenge

--- 01_09b/main.go
+++ 01_09b/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -15,11 +16,63 @@ type Song struct {
 	Name      string `json:"name"`
 	Album     string `json:"album"`
 	PlayCount int64  `json:"play_count"`
+
+	AlbumCount, SongCount int
+}
+
+// A PlaylistHeap is a max-heap of playlistEntries.
+type PlaylistHeap []Song
+
+func (ph PlaylistHeap) Len() int {
+	return len(ph)
+}
+
+func (ph PlaylistHeap) Less(i, j int) bool {
+	return ph[i].PlayCount > ph[j].PlayCount
+}
+
+func (ph PlaylistHeap) Swap(i, j int) {
+	ph[i], ph[j] = ph[j], ph[i]
+}
+
+func (ph *PlaylistHeap) Push(x any) {
+	*ph = append(*ph, x.(Song))
+}
+
+func (ph *PlaylistHeap) Pop() any {
+	original := *ph
+	ln := len(original)
+	lastElement := original[ln-1]
+	*ph = original[0 : ln-1]
+	return lastElement
 }
 
 // makePlaylist makes the merged sorted list of songs
 func makePlaylist(albums [][]Song) []Song {
-	panic("NOT IMPLEMENTED")
+	var playlist []Song
+	pHeap := &PlaylistHeap{}
+	if len(albums) == 0 {
+		return playlist
+	}
+
+	heap.Init(pHeap)
+	for i, f := range albums {
+		firstSong := f[0]
+		firstSong.AlbumCount, firstSong.SongCount = i, 0
+		heap.Push(pHeap, firstSong)
+	}
+	for pHeap.Len() != 0 {
+		p := heap.Pop(pHeap)
+		song := p.(Song)
+		playlist = append(playlist, song)
+		if song.SongCount < len(albums[song.AlbumCount])-1 {
+			nextSong := albums[song.AlbumCount][song.SongCount+1]
+			nextSong.AlbumCount, nextSong.SongCount =
+				song.AlbumCount, song.SongCount+1
+			heap.Push(pHeap, nextSong)
+		}
+	}
+	return playlist
 }
 
 func main() {
